921f4863d84485e3b3f4cccd3544d20874e93e51
gs195 <gsharma.nus@gmail.com>
2020-12-28 11:30:46 +0800

    ⚡️Add create and update note routes

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,13 +1,47 @@
 import NoteModel from "../models/note.model";
 
-const getNote = async (req, res, next) => {
+export const getNote = async (req, res, next) => {
 	try {
-		const foundNote = await NoteModel.find();
-		res.status(200).send(foundNote);
+		const { noteId } = req.params;
+		const foundNote = await NoteModel.find({ _id: noteId });
+		if (!foundNote) throw new Error("no note found for given id");
+		return res.status(200).send(foundNote);
 	} catch (err) {
-		const error = new Error("Unable to get occupant data");
-		return await res.status(400).json(error.message);
+		const error = new Error(`Unable to fetch note: (${err.message})`);
+		error.statusCode = err.statusCode || 400;
+		return next(err);
 	}
 };
 
-export const { getNote };
+export const createNote = async (req, res, next) => {
+	try {
+		const { text } = req.body;
+		if (!text) throw new Error("missing text");
+		const note = new NoteModel({ text });
+		const newNote = await note.save();
+		return res.status(200).send(newNote);
+	} catch (err) {
+		const error = new Error(`Unable to create new note: (${err.message})`);
+		error.statusCode = err.statusCode || 400;
+		return next(err);
+	}
+};
+
+export const updateNote = async (req, res, next) => {
+	try {
+		const { noteId } = req.params;
+		if (!noteId) throw new Error("missing note id");
+		const { text } = req.body;
+		const updatedNote = await NoteModel.findByIdAndUpdate(
+			noteId,
+			{ text },
+			{ new: true }
+		);
+		if (!updatedNote) throw new Error("error in update");
+		return res.status(200).send(updatedNote);
+	} catch (err) {
+		const error = new Error(`Unable to update note: (${err.message})`);
+		error.statusCode = err.statusCode || 400;
+		return next(err);
+	}
+};
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -3,6 +3,8 @@ import * as Ctrl from "../controllers/note.controller";
 
 const router = express.Router();
 
-router.get("/", Ctrl.getNote);
+router.get("/:noteId", Ctrl.getNote);
+router.post("/", Ctrl.createNote);
+router.post("/update/:noteId", Ctrl.updateNote);
 
 export default router;
